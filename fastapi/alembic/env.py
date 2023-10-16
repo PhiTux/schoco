955d9737669f0757d57bdc67e05ef428cf405592
PhiTux <27566312+PhiTux@users.noreply.github.com>
2023-10-16 19:02:32 +0200

    hopefully fixed alembic

--- fastapi/alembic/env.py
+++ fastapi/alembic/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from sqlmodel import SQLModel
-from models_and_schemas import UserCourseLink, User, Course, Project, Homework, EditingHomework
+#from models_and_schemas import UserCourseLink, User, Course, Project, Homework, EditingHomework
 
 from alembic import context
 
--- fastapi/prestart.sh
+++ fastapi/prestart.sh
@@ -3,7 +3,5 @@
 mkdir -p ./data/containers
 mkdir -p ./data/projects
 
-# call alembic upgrades iff /app/data/sql_app.db exists
-if [ -f /app/data/sql_app.db ]; then
-    alembic -c alembic_prod.ini upgrade head
-fi
+# call alembic upgrades
+alembic -c alembic_prod.ini upgrade head
